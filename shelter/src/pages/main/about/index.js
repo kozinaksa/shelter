bb9732d60ecaa548f2b0a92749c511d4398bd0f9
Galina Moroz <kozinaksa@gmail.com>
2023-03-23 23:21:19 +0300

    feat: add about block in main page

---
+++ shelter/src/pages/main/about/index.js
@@ -0,0 +1,7 @@
+import htmlToElements from "../../../utils/htmlToElements";
+import About from './about.html';
+import './about.scss';
+
+const about = htmlToElements(About);
+
+export default about;
--- shelter/src/script.js
+++ shelter/src/script.js
@@ -1,6 +1,8 @@
 import Header from './pages/main/header';
 import Not_only from './pages/main/not-only';
+import About from './pages/main/about';
 
 const main = document.getElementById('main-page');
 main.append(Header);
 main.append(Not_only);
+main.append(About);
