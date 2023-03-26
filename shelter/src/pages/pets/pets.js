13ae55440ba2480e682240b4d17b39a80bf22628
Galina Moroz <kozinaksa@gmail.com>
2023-03-26 17:01:00 +0300

    feat: add footer in pets page

--- shelter/src/pages/pets/pets.js
+++ shelter/src/pages/pets/pets.js
@@ -2,7 +2,9 @@ import '../../styles/_defaultStyle.scss';
 
 import header from "./header";
 import ourFriends from './our-friends';
+import footer from '../index/footer'
 
 const ourPets = document.getElementById("pets-page");
 ourPets.append(header);
 ourPets.append(ourFriends);
+ourPets.append(footer);
