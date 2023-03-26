a841f910a46d476050658353d7872f69b5425ceb
Galina Moroz <kozinaksa@gmail.com>
2023-03-26 16:52:51 +0300

    feat: ass our-friends block in pets page

--- shelter/src/pages/pets/pets.js
+++ shelter/src/pages/pets/pets.js
@@ -1,6 +1,8 @@
 import '../../styles/_defaultStyle.scss';
 
 import header from "./header";
+import ourFriends from './our-friends';
 
 const ourPets = document.getElementById("pets-page");
 ourPets.append(header);
+ourPets.append(ourFriends);
